67b102bce3873a3621450c3ca44e873b4ee33639
DESKTOP-DOGVPUB\SSAFY <skdi6031@naver.com>
2023-08-21 18:00:41 +0900

    0821 algo

---
+++ algo/src/baekjoon/B_13144.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class B_13144 {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        
+    }
+}
